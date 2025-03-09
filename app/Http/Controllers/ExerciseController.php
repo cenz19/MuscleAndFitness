21fe98e9ee39b35db9629623b44af4234bf24e6d
fannyyrr29 <s160422005@student.ubaya.ac.id>
2025-03-09 21:46:03 +0700

    feat: display accordion exercise

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
 use Illuminate\Http\Request;
 
 class ExerciseController extends Controller
 {
-    //
+    
+    function show(){
+        $exercises = Exercise::join('program_exercise as pe', 'pe.exercise_id', '=', 'exercises.id')
+        ->join('programs as p', 'pe.program_id', '=', 'p.id')
+        ->select('exercises.name as exercise_name', 'exercises.description as exercise_description', 
+                    'p.name as program_name', 'p.description as program_description')
+        ->get();
+
+        // return response()->json($exercises);
+        return view('exercise.index', ['exercises' => $exercises]);
+    }
 }
--- resources/views/exercise/index.blade.php
+++ resources/views/exercise/index.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.app');
 
 @section('css')
-    
+    <style>
+        .exercise-description{
+            font-size: 2rem;
+        }
+        .exercise-program{
+            font-size: 1.5rem;
+            font-weight: 600;
+            color: #ff6600;
+        }
+    </style>
 @endsection
 
 @section('content')
-    
+<div class="accordion" id="accordionExample">
+    @for($i = 0; $i < count($exercises); $i++)
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$i}}"
+                aria-expanded="false" aria-controls="collapse{{$i}}">
+                <h3>{{$exercises[$i]->exercise_name}}</h3>
+            </button>
+        </h2>
+        <div id="collapse{{$i}}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <p class="exercise-description">{{$exercises[$i]->exercise_description}}</p>  
+                <p class="exercise-program">Program: {{$exercises[$i]->program_name}}</p>
+            </div>
+        </div>
+    </div>
+    @endfor
+</div>
 @endsection
