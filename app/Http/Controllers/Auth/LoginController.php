d45f8e1c080a4ac92738d31990c9b3177e0b8a80
fannyyrr29 <s160422005@student.ubaya.ac.id>
2025-03-15 22:48:46 +0700

    feat: Creating Login Page

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    
+        // Cari pengguna berdasarkan email
+        $user = \App\Models\User::where('email', $request->email)->first();
+    
+        if(!$user){
+            return back()->withErrors([
+                'error_message' => 'No user found!'
+            ]);
+        }
+        else if ($user && Hash::check($request->password, $user->password)) {
+            // Login berhasil, buat sesi
+            Auth::login($user);
+            return redirect()->intended('/dashboard');
+        }else if(!Hash::check($request->password, $user->password)){
+            return back()->withErrors([
+                'error_message' => 'Wrong password'
+            ]);
+        }
+    
+        // Login gagal, kembali ke halaman login dengan pesan error
+        
+    }
+    
+}
